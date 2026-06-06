591bf0e5051e1221dba5a8251a23b91b289d7ed3
te28teght-glitch <te28teght@gmail.com>
2026-06-06 20:10:05 +0300

    Summ

--- regs.asm
+++ regs.asm
@@ -2,10 +2,6 @@ global Start
 
 section .text
 Start:
-    mov rax, 0x123456789ABCDEF0
-    mov ebx, 0x89ABCDEF
-    mov cx, 0x1234
-    mov dl, 0xAA
-
-    xor eax, eax
+    mov rax, 5
+    add rax, 3
     ret
